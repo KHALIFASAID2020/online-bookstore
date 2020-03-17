49f161980c0fefc8ce8f535c9c16097b1f045785
KHALIFASAID2020 <khalifasaid.2020@gmail.com>
2020-03-17 18:18:42 +0100

    Initial Commit , Created spring boot project and configure the application properties

---
+++ onlinebookstore/src/main/java/in/bushansirgur/onlinebookstore/OnLineBookstoreApplication.java
@@ -0,0 +1,13 @@
+package in.bushansirgur.onlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnLineBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnLineBookstoreApplication.class, args);
+	}
+
+}
